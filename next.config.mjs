0cac88d2ac5624850e4077961e05f16a2afc4ff0
Aaron Heiner <65831660+aheiner2001@users.noreply.github.com>
2026-09-23 12:20:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? (isProd ? '/sunny' : '');
+const isVercel = process.env.VERCEL === '1';
+
+// Vercel uses the root path.
+// GitHub Pages uses /sunny in production.
+const basePath = isVercel
+  ? ''
+  : (process.env.NEXT_PUBLIC_BASE_PATH ?? (isProd ? '/sunny' : ''));
 
 const nextConfig = {
   output: 'export',
